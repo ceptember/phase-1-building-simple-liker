03baae01639d43538d67aef1fbf64dc5118414a9
Christy Perozzi <christy.perozzi@gmail.com>
2021-11-02 10:23:26 -0400

    complete

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let hearts = [...document.getElementsByClassName('like-glyph')];
+hearts.forEach((heart) => {heart.addEventListener('click', handleLike) } )
+ 
+function handleLike (event){
+  //console.log(event.target);
+  mimicServerCall()
+    .then(() => {
+      //console.log('then!');
+      if ( event.target.textContent == FULL_HEART ){
+        event.target.textContent = EMPTY_HEART;
+        event.target.className = "like-glyph";
+      }
+      else {
+        event.target.textContent = FULL_HEART;
+        event.target.className = "like-glyph activated-heart";
+      }
+      
+      
+    })
 
+    .catch(error => {
+      document.querySelector('#modal').className='';
+      document.querySelector('#modal').textContent = error;
+      setTimeout( () => {document.querySelector('#modal').className='hidden';}, 3000)
+    })
+}
 
 
 
